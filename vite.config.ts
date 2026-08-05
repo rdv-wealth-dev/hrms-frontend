08467de2c445d1035df44491ce0a387be7831661
Shikhar sharwalia <shikhar.sharwalia@redvisionglobal.com>
2026-08-05 17:40:14 +0530

    fix the vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-
+ 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
@@ -14,4 +14,3 @@ export default defineConfig({
     },
   },
 });
-
